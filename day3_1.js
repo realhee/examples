65dd4673c93ddd25cd2373d10255c20bf48869de
genie <wlsgml5628@gmail.com>
2020-10-08 00:53:41 +0900

    day3_1 - 가운데 글자 가져오기

--- day3_1.js
+++ day3_1.js
@@ -2,3 +2,11 @@
 // 가운데 글자 가져오기
 
 
+function solution(s) {
+    var num = Math.floor(s.length/2);
+    if (s.length % 2 == 0) {
+        return s[num-1]+s[num];
+    } else {
+        return s[num]; 
+    }
+}
