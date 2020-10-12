5e3846c860a25965a246992eed32e77716af2851
genie <wlsgml5628@gmail.com>
2020-10-12 20:26:38 +0900

    day7_5 - 콜라츠 추측

---
+++ day7_5.js
@@ -0,0 +1,19 @@
+// day7_5
+// 콜라츠 추측
+
+function solution(num) {
+    var answer = 0;
+    var count = 0;
+    while (num != 1) {
+        if(num % 2 === 0) {
+            num = num / 2;
+            count++;
+        }
+        else {
+            num = (num*3) + 1;
+            count++;
+        }
+        if (count == 500) return -1;
+    }
+    return count;
+}
