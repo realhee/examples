c666eab2afbcd0fca0d2330fe4b7d366ab3c3130
genie <wlsgml5628@gmail.com>
2020-11-07 00:24:03 +0900

    day18_1 - 카펫

---
+++ day18_1.js
@@ -0,0 +1,11 @@
+// day18_1
+// 카펫
+
+function solution(brown, red) {
+    var n = 0;
+    for(var m = red; m >= 1; m--){
+        n = Math.floor(red / m);
+        if(red % m == 0 && ((m + 2) * (n + 2) == (brown + red)))
+            return [m + 2, n + 2]
+    }
+}
