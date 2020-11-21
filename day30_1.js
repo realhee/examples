ac0954725f92379b0dae11713f9c7803afaae448
genie <wlsgml5628@gmail.com>
2020-11-21 22:43:18 +0900

    day30_1 - 예상 대진표

--- day30_1.js
+++ day30_1.js
@@ -1,3 +1,16 @@
 // day 30_1
-// 
+// 예상 대진표
 
+function solution(n,a,b){
+    var answer = 0;
+    var numA = a;
+    var numB = b;
+    
+  	while(numA!=numB){
+      numA = Math.ceil(numA /2);
+      numB = Math.ceil(numB /2);
+      answer = answer + 1;
+  }
+
+  return answer
+}
