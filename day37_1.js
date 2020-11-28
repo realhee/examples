738bd3bb54609b413d33c2786dd2b9bcae0c5d09
genie <wlsgml5628@gmail.com>
2020-11-28 22:33:06 +0900

    day37_1 - 땅따먹기 (풀이 2)

--- day37_1.js
+++ day37_1.js
@@ -1,34 +1,27 @@
 // day 37_1
 // 땅따먹기
 
-function solution(land){
-	var answer = 0
-    var one = 0
-    var two = 0
-    var three = 0
-    for(var i=1; i<land.length; i++){
-    	for(var j=0 j<4; j++){
-        	if(j==0){
-            	one = land[i-1][j+1] + land[i][j]
-                two = land[i-1][j+2] + land[i][j]
-                three = land[i-1][j+3] + land[i][j]
-            } else if(j==1){
-            	one = land[i-1][j-1] + land[i][j]
-                two = land[i-1][j+1] + land[i][j]
-                three = land[i-1][j+2] + land[i][j]
-            } else if (j==2){
-            	one = land[i-1][j-2] + land[i][j]
-                two = land[i-1][j-1] + land[i][j]
-                three = land[i-1][j+1] + land[i][j]
-            } else {
-            	one = land[i-1][j-3] + land[i][j]
-                two = land[i-1][j-2] + land[i][j]
-                three = land[i-1][j-1] + land[i][j]	
-            }
-          
-          land[i][j] = Math.max(one, two, three)
+function solution(land) {
+    var answer = 0;
+    let before = 0;
+
+    for(let i=0 ; i<land.length-1 ; i++){
+        land[i+1] = max(land[i],land[i+1]);
+    }
+    answer = Math.max(...land[land.length-1])
+    return answer;
+}
+
+function max(arr1,arr2){
+    let stack = [];
+    let newArr = [0,0,0,0];
+    for(let i=0 ; i<arr2.length ; i++){
+        for(let j=0 ; j<arr1.length ; j++){
+            if(j === i) continue;
+            stack.push(arr1[j]+arr2[i])
         }
-    } 
-  answer = Math.max.apply(null, land[land.length-1])
-  return answer
+        newArr[i] = Math.max(...stack);
+        stack = [];
+    }
+    return newArr;
 }
