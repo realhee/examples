96ce347a85b060d4cae4e9199aa86330efddb57a
realhee <wlsgml5628@gmail.com>
2020-12-04 15:16:00 +0900

    day41_1 - 피보나치 수 (두 번째 풀이)

--- day40_1.js
+++ day40_1.js
@@ -15,3 +15,11 @@ function solution(num){
     answer = memo[num];
     return answer;
   }
+
+  /*
+  // solution 2
+    function fibonacci(num) {
+    if(num < 2) return num;
+    return fibonacci(num-1) + fibonacci(num-2);
+  }
+  */
