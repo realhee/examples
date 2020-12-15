9834ebf5f700cdf535dcca68b18a51a69705e705
genie <wlsgml5628@gmail.com>
2020-12-15 22:16:51 +0900

    day49_1 - N으로 표현

---
+++ day49_1.js
@@ -0,0 +1,23 @@
+// day 49_1
+// N으로 표현
+
+function solution(N, number) {
+    const set = new Array(8).fill().map(() => new Set());
+    
+    for(let i=0; i<8; i++){
+        set[i].add(Number(N.toString().repeat(i+1)));
+        console.log(set[-1])
+        for (let j=0; j < i; j++) {
+            for(const arg1 of set[j]){
+                for(const arg2 of set[i-j-1]){
+                    set[i].add(arg1+arg2);
+                    set[i].add(arg1-arg2);
+                    set[i].add(arg1*arg2);
+                    set[i].add(arg1/arg2);
+                }
+            }
+        }
+        if(set[i].has(number)) return i+1;
+    }
+    return -1;
+}
