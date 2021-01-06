ce588602f238b1b767db246afe6d7d919bb6a873
genie <wlsgml5628@gmail.com>
2021-01-06 21:37:59 +0900

    day70_1 - 최고의 집합

---
+++ day70_1.js
@@ -0,0 +1,16 @@
+// day 70_1
+// 최고의 집합
+
+function solution(n, s) {
+    var answer = [];
+    if (n > s) {
+      return [-1];
+    }
+  
+    for (let i = 0; i < n; i++) {
+      const number = Math.round(s/(n-i));
+      answer.push(number);
+      s = s - number;
+    }
+    return answer;
+}
