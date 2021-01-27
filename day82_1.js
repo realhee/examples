2374294b1b4da709c4a0ae41f2449a867e5debca
genie <wlsgml5628@gmail.com>
2021-01-27 22:12:55 +0900

    day82_1 - 올바른 괄호 (풀이 추가)

--- day82_1.js
+++ day82_1.js
@@ -1,6 +1,6 @@
 // day 82_1
 // 올바른 괄호
-
+/*
 function solution(s) {
     if(s[s.length -1]!=")")
         return false;
@@ -13,4 +13,26 @@ function solution(s) {
             return false;
     }
     return (result)? false : true;
+}
+*/
+// 2번째 풀이
+function solution(s){
+    var answer = true;
+    let left = 0;
+    let right = 0;
+    for(let i=0; i< s.length  ; i++){
+        if(s[i]==="("){
+            left++;
+        }
+        if(s[i]===")"){
+            right++;
+        }
+        if(right > left ){
+            return false;
+        }
+    }
+    if(left!==right){
+        return false;
+    }
+    return true;
 }
