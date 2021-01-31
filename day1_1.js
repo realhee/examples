9a83e1159b75ac1b9f5abbcde260c43c18faf68f
genie <wlsgml5628@gmail.com>
2021-01-31 21:11:07 +0900

    파일 정리

--- day1_1.js
+++ day1_1.js
@@ -30,3 +30,29 @@ function solution(board, moves) {
     })
     return result;
 }
+
+// 두번째 풀이 (for문 형태)
+/*
+function solution(board, moves) {
+    var count =0;
+    var stack = [];
+    for(var i=0;i<moves.length;i++){
+        var now = moves[i]-1;
+        for(var j=0;j<board.length;j++){
+            if(board[j][now]!=0){
+                if(stack[stack.length-1]===board[j][now]){
+                    stack.pop();
+                    count+=2;
+                }
+                else{
+                    stack.push(board[j][now]);
+                }
+                board[j][now] = 0;
+                break;
+            }
+        }
+    }
+    console.log(stack);
+    return count;
+}
+*/
