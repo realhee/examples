3a3fbec94bf24083859ec0d197596e7cb0732ded
genie <wlsgml5628@gmail.com>
2021-02-03 21:04:11 +0900

    불필요한 변수 삭제

--- day85_1.js
+++ day85_1.js
@@ -3,14 +3,11 @@
 
 function solution(A) {
     for(var i=0; i<A.length; i++) {
-        var count = 0;
         var index = A.indexOf(A[i]);
         
         while(index > -1) {
-        count++;
-        A.splice(index, 1)
-        index = A.indexOf(A[i])
-        console.log(A[i], count)
+            A.splice(index, 1)
+            index = A.indexOf(A[i])
         }
     }
 }
