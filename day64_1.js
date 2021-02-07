9480dcf8fa2e501e0f7ea3eda22f02a263ceb300
genie <wlsgml5628@gmail.com>
2021-02-07 21:33:23 +0900

    day 64_1 - 거스름 돈 (두 번째 풀이)

--- day64_1.js
+++ day64_1.js
@@ -1,6 +1,6 @@
 // day 64_1
 // 거스름돈
-
+/*
 function solution(n, money) {
     let dp = [1];
     for(let i = 0 ; i < n ; ++i) dp.push(0);
@@ -13,4 +13,20 @@ function solution(n, money) {
     });
         
     return dp[n];
+}
+*/
+
+
+// 두 번째 풀이
+function solution(n, money) {
+    const cache = [];
+    for (let i = 0; i <= n; i++){
+        cache[i] = i % money[0] ? 0 : 1;
+    }
+    for (let i = 1; i < money.length; i++){
+        for(let j = money[i]; j <= n; j++){
+            cache[j] += cache[j - money[i]];
+        }
+    }
+    return cache[n] % 1000000007;
 }
